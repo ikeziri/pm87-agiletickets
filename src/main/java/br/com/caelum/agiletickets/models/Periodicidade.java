238623ab2413f843092b7e345bfd95643d98a8e9
Andre <ikeziri@gmail.com>
2015-08-14 09:46:48 -0300

    ajuste da sessao sem teste

--- src/main/java/br/com/caelum/agiletickets/models/Periodicidade.java
+++ src/main/java/br/com/caelum/agiletickets/models/Periodicidade.java
@@ -2,6 +2,16 @@ package br.com.caelum.agiletickets.models;
 
 public enum Periodicidade {
 	
-	DIARIA, SEMANAL;
+	DIARIA(1), SEMANAL(7);
+	private int value;
+	private Periodicidade(int value) {
+		this.setValue(value);
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
 	
 }
